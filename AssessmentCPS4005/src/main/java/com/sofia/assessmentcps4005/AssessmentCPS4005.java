6def41f2d5ccb0780a9405879dfa0fd0ec995aab
Sofia Lavrekhina <sofi0702@icloud.com>
2025-03-17 14:14:51 +0000

    git repository created and connected

---
+++ AssessmentCPS4005/src/main/java/com/sofia/assessmentcps4005/AssessmentCPS4005.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sofia.assessmentcps4005;
+
+/**
+ *
+ * @author soflavre
+ */
+public class AssessmentCPS4005 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

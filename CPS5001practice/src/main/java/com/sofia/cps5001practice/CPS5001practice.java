eb06a012f25ee5c8143fb47ebf59b915b906d76d
Sofia Lavrekhina <sofi0702@icloud.com>
2025-09-22 14:13:43 +0100

    added some design to fxml files

---
+++ CPS5001practice/src/main/java/com/sofia/cps5001practice/CPS5001practice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sofia.cps5001practice;
+
+/**
+ *
+ * @author soflavre
+ */
+public class CPS5001practice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
